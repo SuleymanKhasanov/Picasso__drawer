c2fc6302e74bc309f99551e0d762bf4cf0c6ba1c
Сулейман Хасанов <macbook@MacBook-Air.local>
2023-06-30 10:46:31 +0500

    Добавлена функция изменения цвета и толщины линии

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.height = 700;
 canvas.width = 600;
@@ -29,9 +31,25 @@ function onMouseUp(event) {
   painting = false;
 }
 
+function hendleColorClick(event) {
+  let colors = event.target.style.backgroundColor;
+  ctx.strokeStyle = colors;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", onMouseUp);
+}
+
+Array.from(colors).forEach((element) => element.addEventListener("click", hendleColorClick));
+
+function hendleRangeChange(event) {
+  let size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+if (range) {
+  range.addEventListener("input", hendleRangeChange);
 }
