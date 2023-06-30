c4c4ed1d6943ea87281adc0d09a88b02faf03385
Сулейман Хасанов <macbook@MacBook-Air.local>
2023-06-30 14:13:47 +0500

    Добавлена функция заливки

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("mode");
 
 canvas.height = 700;
 canvas.width = 600;
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = "#000";
+ctx.fillStyle = "#000";
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(event) {
   let x = event.offsetX;
@@ -34,6 +37,13 @@ function onMouseUp(event) {
 function hendleColorClick(event) {
   let colors = event.target.style.backgroundColor;
   ctx.strokeStyle = colors;
+  ctx.fillStyle = colors;
+}
+
+function hendleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, 600, 700);
+  }
 }
 
 if (canvas) {
@@ -41,6 +51,7 @@ if (canvas) {
   canvas.addEventListener("mousedown", onMouseDown);
   canvas.addEventListener("mouseup", onMouseUp);
   canvas.addEventListener("mouseleave", onMouseUp);
+  canvas.addEventListener("click", hendleCanvasClick);
 }
 
 Array.from(colors).forEach((element) => element.addEventListener("click", hendleColorClick));
@@ -52,4 +63,16 @@ function hendleRangeChange(event) {
 
 if (range) {
   range.addEventListener("input", hendleRangeChange);
+}
+
+function hendleModeClick() {
+  if (filling === true) {
+    filling = false;
+  } else {
+    filling = true;
+  }
+}
+
+if (mode) {
+  mode.addEventListener("click", hendleModeClick);
 }
